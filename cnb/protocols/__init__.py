8e078fac85feef9336657c5a3130867b9d2c436b
danielmarchan3 <danielmarchan3@gmail.com>
2021-09-16 10:25:28 +0200

    protocol set spiral position created not finished

--- cnb/protocols/__init__.py
+++ cnb/protocols/__init__.py
@@ -5,3 +5,4 @@
 # **************************************************************************
 from .protocol_writePhaseShift import cnbWritePhaseShift
 from .protocol_reportParameters import cnbReportParameters
+from .protocol_setSpiralPosition import CNBProtMovieSpiralPosition
