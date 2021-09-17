8c8b3e924bafa3777cd4d8a083a384ffe71af42a
danielmarchan3 <danielmarchan3@gmail.com>
2021-09-17 11:10:10 +0200

    protocol set Spiral Position ready to test

--- cnb/protocols/protocol_setSpiralPosition.py
+++ cnb/protocols/protocol_setSpiralPosition.py
@@ -23,19 +23,22 @@
 # *  e-mail address 'scipion@cnb.csic.es'
 # *
 # **************************************************************************
-
-from pwem.protocols.protocol import ProtProcessMovies
-from pwem.objects import SetOfCTF, Set
+from pwem.protocols.protocol import EMProtocol
+from pwem.protocols.protocol_movies import ProtProcessMovies
+from pyworkflow import VERSION_3_0
+from pwem.objects import Set
 from pyworkflow.protocol import params, STATUS_NEW
-from pyworkflow.utils import Message
-from ..constants import *
-import pyworkflow.protocol.constants as pwcts
-import os, pickle
+import os
 from datetime import datetime
 from pyworkflow.utils.properties import Message
-from pyworkflow.utils.path import moveFile
-from pwem.emlib.image import ImageHandler
-from pwem.objects import SetOfMovies, Movie
+from pwem.objects import SetOfMovies, Movie, SetOfMicrographs
+from pyworkflow.protocol.params import (PointerParam, LEVEL_ADVANCED)
+from pyworkflow.protocol import STEPS_PARALLEL
+import pyworkflow.utils as pwutils
+import pwem.objects as emobj
+from xmipp3.convert import getScipionObj
+import pyworkflow.protocol.constants as cons
+
 
 
 class CNBProtMovieSpiralPosition(ProtProcessMovies):
@@ -47,20 +50,18 @@ class CNBProtMovieSpiralPosition(ProtProcessMovies):
     The same criteria is used for assigning the gain to the output movies (external corrected > external > estimated)
     """
     _label = 'movie spiral position'
-    _lastUpdateVersion = VERSION_1_1
+    _lastUpdateVersion = VERSION_3_0
 
 
     def __init__(self, **args):
-        EMProtocol.__init__(self, **args)
+        ProtProcessMovies.__init__(self, **args)
         self.stepsExecutionMode = STEPS_PARALLEL
-        self.estimatedIds = []
         self.spirals = {}
 
     # -------------------------- DEFINE param functions ----------------------
     def _defineParams(self, form):
         form.addSection(label=Message.LABEL_INPUT)
-        form.addParam('inputMovies', PointerParam, pointerClass='SetOfMovies, '
-                                                                'Movie',
+        form.addParam('inputMovies', PointerParam, pointerClass='SetOfMovies',
                       label=Message.LABEL_INPUT_MOVS,
                       help='Select movies')
 
@@ -69,6 +70,23 @@ class CNBProtMovieSpiralPosition(ProtProcessMovies):
         form.addParallelSection(threads=4, mpi=1)
 
     # -------------------------- STEPS functions ------------------------------
+
+    def _insertAllSteps(self):
+        # Build the list of all processMovieStep ids by
+        # inserting each of the steps for each movie
+        self.insertedDict = {}
+        self.samplingRate = self.inputMovies.get().getSamplingRate()
+        self.micsFn =  self._getMicsPath()
+        self.hasAlignment = self.micsFn != None
+        self.acquisition = self.inputMovies.get().getAcquisition()
+        pwutils.makePath(self._getExtraPath('DONE'))
+
+        movieSteps = self._insertNewMoviesSteps(self.insertedDict,
+                                                self.inputMovies.get())
+        finalSteps = self._insertFinalSteps(movieSteps)
+        self._insertFunctionStep('createOutputStep',
+                                 prerequisites=finalSteps, wait=True)
+
     def createOutputStep(self):
         pass
 
@@ -88,19 +106,25 @@ class CNBProtMovieSpiralPosition(ProtProcessMovies):
 
         return deps
 
+    def _insertMovieStep(self, movie):
+        """ Insert the processMovieStep for a given movie. """
+        # Note1: At this point is safe to pass the movie, since this
+        # is not executed in parallel, here we get the params
+        # to pass to the actual step that is gone to be executed later on
+        # Note2: We are serializing the Movie as a dict that can be passed
+        # as parameter for a functionStep
+        movieDict = movie.getObjDict(includeBasic=True)
+        movieStepId = self._insertFunctionStep('processMovieStep',
+                                               movieDict,
+                                               movie.hasAlignment(),
+                                               prerequisites=[])
+        return movieStepId
+
 
     def _processMovie(self, movie):
         movieId = movie.getObjId()
-
-        print('entro aqui siendo la movie a procesar:')
-        print(movieId)
-       # if movieId not in self.estimatedIds:
-        self.estimatedIds.append(movieId)
         spiral_position = self.getSpiralPosition(movie)
-        
         self.spirals[movieId] = spiral_position
-        
-        #self.estimatedIds2.append(movieId)
 
         fnSummary = self._getPath("summary.txt")
         fnMonitorSummary = self._getPath("summaryForMonitor.txt")
@@ -111,18 +135,22 @@ class CNBProtMovieSpiralPosition(ProtProcessMovies):
             fhSummary = open(fnSummary, "a")
             fnMonitorSummary = open(fnMonitorSummary, "a")
 
-        #fhSummary.write("movie_%06d_poisson_count: mean=%f std=%f [min=%f,max=%f]\n" %
-         #               (movieId, stats['mean'], stats['std'], stats['min'], stats['max']))
+        fhSummary.write("movie_%06d: spiral position=%d\n" %
+                        (movieId, spiral_position))
 
         fhSummary.close()
         fnMonitorSummary.close()
 
     def getSpiralPosition(self, movie):
-        print(movie.getFileName())
         movieFn = movie.getFileName()
-        
-    
-        return movieFn
+        x = movieFn.find("movie_")
+        substr = movieFn[x+6:]
+        x1 = substr.find("_")
+        spiral = substr[x1+1:x1+6]
+        print(movie.getFileName())
+        print(int(spiral))
+
+        return int(spiral)
 
 
     def _loadOutputSet(self, SetClass, baseName):
@@ -141,17 +169,13 @@ class CNBProtMovieSpiralPosition(ProtProcessMovies):
             outputSet = SetClass(filename=setFile)
             outputSet.setStreamState(outputSet.STREAM_OPEN)
 
-            if isinstance(self.inputMovies.get(), SetOfMovies) or isinstance(self.inputMovies.get(), Movie):
-                inputMovies = self.inputMovies.get()
-                outputSet.copyInfo(inputMovies)
-
         return outputSet
 
-
-    #def _checkNewInput(self):
-     #   if isinstance(self.inputMovies.get(), SetOfMovies):
-      #      ProtProcessMovies._checkNewInput(self)
-
+    def _stepsCheck(self):
+        # Input movie set can be loaded or None when checked for new inputs
+        # If None, we load it
+        self._checkNewInput()
+        self._checkNewOutput()
 
     def _checkNewInput(self):
         # Check if there are new movies to process from the input set
@@ -170,10 +194,8 @@ class CNBProtMovieSpiralPosition(ProtProcessMovies):
         self.lastCheck = now
         # Open input movies.sqlite and close it as soon as possible
         self._loadInputList()
-        
-        newMovies = [m.getObjId() not in self.insertedDict
-                        for m in self.listOfMovies]
-        
+
+        newMovies = [movie.clone() for movie in self.listOfMovies if movie.getObjId() not in self.insertedDict]
         outputStep = self._getFirstJoinStep()
 
         if newMovies:
@@ -184,12 +206,7 @@ class CNBProtMovieSpiralPosition(ProtProcessMovies):
 
             self.updateSteps()
 
-    def _stepsCheck(self):
-        # Input movie set can be loaded or None when checked for new inputs
-        # If None, we load it
-        self._checkNewInput()
-        #self._checkNewOutput()
-
+   
     def _loadInputList(self):
         """ Load the input set of movies and create a list. """
         moviesFile = self.inputMovies.get().getFileName()
@@ -205,70 +222,62 @@ class CNBProtMovieSpiralPosition(ProtProcessMovies):
     def _checkNewOutput(self):
         if getattr(self, 'finished', False):
             return
-        if isinstance(self.inputMovies.get(), Movie):
-            movie = self.inputMovies.get()
+
+        # Load previously done items (from text file)
+        doneList = self._readDoneList()
+        # Check for newly done items
+        newDone = [m.clone() for m in self.listOfMovies
+                   if int(m.getObjId()) not in doneList and
+                   self._isMovieDone(m)]
+
+        allDone = len(doneList) + len(newDone)
+        # We have finished when there is not more input movies
+        # (stream closed) and the number of processed movies is
+        # equal to the number of inputs
+        self.finished = self.streamClosed and \
+                        allDone == len(self.listOfMovies)
+        streamMode = Set.STREAM_CLOSED if self.finished \
+                     else Set.STREAM_OPEN
+
+        if newDone:
+            self._writeDoneList(newDone)
+        elif not self.finished:
+            # If we are not finished and no new output have been produced
+            # it does not make sense to proceed and updated the outputs
+            # so we exit from the function here
+            return
+
+        moviesSet = self._loadOutputSet(SetOfMovies, 'movies.sqlite')
+        
+        if self.hasAlignment:
+            micSet = self._loadOutputSet(SetOfMicrographs, 'micrographs.sqlite')
+            inputMicSet = self._loadInputMicrographSet(self.micsFn)
+
+        for movie in newDone:
             movieId = movie.getObjId()
-            streamMode = Set.STREAM_CLOSED
-            saveMovie = self.getAttributeValue('doSaveMovie', False)
-            moviesSet = self._loadOutputSet(SetOfMovies, 'movies.sqlite')
-            moviesSet.append(movie)
+            newMovie = movie.clone()
+            spiral = self.spirals[movieId]
+            setAttribute(newMovie, '_SPIRAL_POSITION', spiral)
+            moviesSet.append(newMovie)
+            if self.hasAlignment:
+                mic = inputMicSet[movieId].clone()
+                setAttribute(mic, '_SPIRAL_POSITION', spiral)
+                micSet.append(mic)
 
-            self._updateOutputSet('outputMovies', moviesSet, streamMode)
+        moviesSet.setAcquisition(self.acquisition.clone())
+        moviesSet.setSamplingRate(self.samplingRate)
+        #    outputSet.copyInfo(inputMovies)
+        self._updateOutputSet('outputMovies', moviesSet, streamMode)
+        
+        if self.hasAlignment:
+            micSet.setAcquisition(self.acquisition.clone())
+            micSet.setSamplingRate(self.samplingRate)
+            self._updateOutputSet('outputMicrographs', micSet, streamMode)
+
+        if self.finished:  # Unlock createOutputStep if finished all jobs
             outputStep = self._getFirstJoinStep()
-            outputStep.setStatus(cons.STATUS_NEW)
-            self.finished = True
-        else:
-            # Load previously done items (from text file)
-            doneList = self._readDoneList()
-            # Check for newly done items
-            newDone = [m.clone() for m in self.listOfMovies
-                       if int(m.getObjId()) not in doneList and
-                       self._isMovieDone(m)]
-
-            allDone = len(doneList) + len(newDone)
-            # We have finished when there is not more input movies
-            # (stream closed) and the number of processed movies is
-            # equal to the number of inputs
-            self.finished = self.streamClosed and \
-                            allDone == len(self.listOfMovies)
-            streamMode = Set.STREAM_CLOSED if self.finished \
-                else Set.STREAM_OPEN
-
-            if newDone:
-                self._writeDoneList(newDone)
-            elif not self.finished:
-                # If we are not finished and no new output have been produced
-                # it does not make sense to proceed and updated the outputs
-                # so we exit from the function here
-                return
-
-            moviesSet = self._loadOutputSet(SetOfMovies, 'movies.sqlite')
-
-            for movie in newDone:
-                movieId = movie.getObjId()
-                newMovie = movie.clone()
-                
-                # Echar un ojo a esta lista
-                if movieId in self.estimatedIds2:
-                    stats = self.stats[movieId]
-                    mean = stats['mean']
-                    std = stats['std']
-                    min = stats['min']
-                    max = stats['max']
-
-                    setAttribute(newMovie, '_MEAN_DOSE_PER_FRAME', mean)
-                    setAttribute(newMovie, '_STD_DOSE_PER_FRAME', std)
-                    setAttribute(newMovie, '_MIN_DOSE_PER_FRAME', min)
-                    setAttribute(newMovie, '_MAX_DOSE_PER_FRAME', max)
-
-                moviesSet.append(newMovie)
-
-            self._updateOutputSet('outputMovies', moviesSet, streamMode)
-
-            if self.finished:  # Unlock createOutputStep if finished all jobs
-                outputStep = self._getFirstJoinStep()
-                if outputStep and outputStep.isWaiting():
-                    outputStep.setStatus(cons.STATUS_NEW)
+            if outputStep and outputStep.isWaiting():
+                outputStep.setStatus(cons.STATUS_NEW)
 
 
     def _updateOutputSet(self, outputName, outputSet, state=Set.STREAM_OPEN):
@@ -290,18 +299,34 @@ class CNBProtMovieSpiralPosition(ProtProcessMovies):
         outputSet.close()
 
 
-
     # ------------------------- UTILS functions --------------------------------
-    
+    def _getMicsPath(self):
+        prot1 = self.inputMovies.getObjValue()  # pointer to previous protocol
+
+        if hasattr(prot1, 'outputMicrographs'):
+            path1 = prot1.outputMicrographs.getFileName()
+            if os.path.getsize(path1) > 0:
+                return path1
+
+        elif hasattr(prot1, 'outputMicrographsDoseWeighted'):
+            path2 = prot1.outputMicrographsDoseWeighted.getFileName()
+            if os.path.getsize(path2) > 0:
+                return path2
+
+        else:
+            return None
+
+    def _loadInputMicrographSet(self, micsFn):
+        self.debug("Loading input db: %s" % micsFn)
+        micSet = SetOfMicrographs(filename=micsFn)
+        micSet.loadAllProperties()
+        return micSet
 
     # --------------------------- INFO functions -------------------------------
     def _validate(self):
         errors = []
         if errors:
-            errors.append("An experimental gain can be associated with a "
-                          "setOfMovies during its importing protocol. "
-                          "Otherwise, no gain reorientation nor "
-                          "gain normalization can be performed.")
+            pass
         return errors
 
     def _summary(self):
